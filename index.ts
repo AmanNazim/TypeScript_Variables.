9a49e01772cf5aa527d34afac8b6fadaedb805ce
AmanNazim <aman.nazim07@gmail.com>
2024-03-04 12:47:58 +0500

    Files Updated

---
+++ index.ts
@@ -0,0 +1,165 @@
+//Creating Bio Data using varibles in TypeScript.
+
+//First storing all the data in variables for printing.
+/*Declaring const variable name and declared its type string. to store permanent name in the form of
+string in Bio Data.*/
+const fullName: string = "Aman Nazim";//Aman.
+
+/*Declaring const fatherName variable to store permanent father name data in the form of string.*/
+const fatherName: string = "Nazim";//Nazim.
+
+//Declaring let age variable to store age data using number data type.
+let age: number = 16;//16.
+
+//Declaring const gender vairable to store permanent data gender using string data type.
+const gender: string = "Male";//Male.
+
+//Declaring const religion variable to store permanent data of religion in a variable.
+const religion: string = "Islam";//Islam.
+
+//Declaring const country variable to store permanent country data in a variable using string data Type. 
+const country: string = "Pakistan";//Pakistan.
+
+//Declaring const city variable to store name of city in a variable using string data type.
+const city: string = "Karachi";//Karachi.
+
+//Declaring let student variable to store a boolean value of student in a variable in true.
+let isStudent: boolean = true;//true.
+
+//Declaring class name of a student using let className variable of string data type to store class.
+let className: string = "HSC Part 2";//HSC Part 2.
+
+//Declaring classGroup variable to store a group of class using string data type.
+let classGroup: string = "Pre-Medical";//Pre-Medical.
+
+//Declaring let educationalInstitute variable to store Institute name in string data type.
+let educationalInstitute: string = "College";//College.
+
+//Decalring let job variable to store a false boolean value in a variable.
+let isEmployed: boolean = false;//false.
+
+//Declaring let maritalStatus value to store maritalstatus value in string data type.
+let maritalStatus: string = "Single";//Single.
+
+//Declaring let course variable to define a course in a variable in string data type.
+const courseName: string = "Cloud Applied Generative AI Engineer";//Cloud Applied Generative AI Engineer.
+
+//Declaring let courseQuarter variable to store Quarter 1 value in string data type.
+let courseQuarter: string = "Quarter 1";//Quarter 1.
+
+//Declaring let courseInstitute variable to store course institute name in a variable of string data type.
+let courseInstitute: string = "Governer House Karachi";//Governer House Karachi.
+
+//Updating educationalInstitute variable to store a proper name of institute in a variable.
+educationalInstitute = "Goverment Degree Boys College";//Goverment Degree Boys College.
+
+//Declaring let address variable in string data type and store proper address data in a variable.
+let address: string = "Agha Manzil, 1st floor, flat no 102, near Rehmat Masjid, bhimpura, karachi.";
+//Agha Manzil, 1st floor, flat no 102, near Rehmat Masjid, bhimpura, karachi.
+
+//Declaring let emailAddress variable to store email information in a variable using string data type.
+let emailAddress: string = "aman.nazim07@gmail.com";//aman.nazim07@gmail.com.
+
+//Declaring let contact variable to store data of contact information using string data type.
+let contact: string = "0303 5191793";//0303 5191793.
+
+//Declaring let whatsappNumber variable to store whatsapp number in a variable of string data type.
+let whatsappNumber: string = "+92 3035191793";//+92 3035191793.
+
+//Declaring let hobbies variable to store Hobbies in it using string data type.
+let hobbies: string = "Hobbies:";//Hobbies.
+
+//Declaring let hobby1 variable to store a data of hobby name using string data type.
+let hobby1: string = "To learn about Technology.";//To learn about Technology.
+
+//Declaring let hobby2 variable to store a data of another hobby name using data type.
+let hobby2: string = "Operating Computer.";//Operating computer.
+
+//Declaring let hobby3 variable to store another hobby name using string data type.
+let hobby3: string = "Cooking.";//Cooking.
+
+//Declaring let youtubeChannel variable to store a channel name using string data type in a variable. 
+let youtubeChannel: string = "The Great Learner";//The Great Learner.
+
+//Declaring let githubAccount variable to store a github account name in a variable using string data type. 
+let githubAccount: string = "Aman Nazim"//Aman Nazim.
+
+/*Printing all the variables using literal templates in console.log to print Data and add additional 
+context with data.*/
+//Accessing variable name to print a value of name after writing "Name" in literal templates.
+console.log(`Name: ${fullName}.`);//Name: Aman.
+
+//First writing "Fahter Name" and then Accessing variable fatherName to print a value of it.
+console.log(`Father Name: ${fatherName}.`);//Father Name: Nazim.
+
+//First writing "Age" and then Accessing age variable to print a age value to print it.
+console.log(`Age: ${age}.`);//Age: 16.
+
+//First writing "Gender" and accessing value of variable gender to print it.
+console.log(`Gender: ${gender}.`);//Gender: Male.
+
+//Writing "Religion" and accessing a value of religion variable to print it.
+console.log(`Religion: ${religion}.`);//Religion: Islam.
+
+//Writing "Country" and accessing a country variable value to print it.
+console.log(`Country: ${country}.`);//Country: Pakistan.
+
+//Writing "City" and accessing city variable for its value to print it .
+console.log(`City: ${city}.`);//City: Karachi.
+
+//Writing "Student" and accessing isStudent variable value to print it.
+console.log(`Student: ${isStudent}.`);//Student: True.
+
+//Writing "Class Name" and accessing variable className to get its value to print it.
+console.log(`Class Name: ${className}.`);//Class Name: HSC Part 2.
+
+//Writing "Group Name" and accessing classGroup variable value to print it.
+console.log(`Group Name: ${classGroup}.`);//Group Name: Pre-Medical.
+
+//Writing "Institute Name" and accessing educationalInstitute variable to print it.
+console.log(`Institute Name: ${educationalInstitute}.`);//Institute Name: Goverment Degree Boys College.
+
+//Writing "Employed" and accessing isEmployed variable value to print it.
+console.log(`Employed: ${isEmployed}.`);//Employed: False.
+
+//Writing "Marital Status" and accessing maritalStatus variable to print it.
+console.log(`Marital Status: ${maritalStatus}.`);//Marital Status: Single.
+
+//Writing "Course Name" and accessing courseName variable to print a value of it.
+console.log(`Course Name: ${courseName}.`);//Course Name: Cloud Applied Generative AI Engineer.
+
+//Writing "Course Quarter" and accessing courseQuarter variable to print its value.
+console.log(`Course Quarter: ${courseQuarter}.`);//Course Quarter: Quarter 1.
+
+//Writing "Institute of Course" and accessing courseInstitute varaiable to print a value of it.
+console.log(`Institute of Course: ${courseInstitute}.`);//Institute of Course: Governer House Karachi.
+
+//Writing "Address" and accessing address varaiable to print a value of it.
+console.log(`Address: ${address}.`);//Address: Agha Manzil, 1st floor, flat no 102, near Rehmat Masjid, bhimpura, karachi.
+
+//Writing "Email Address" and accessing emailAddress varaiable to print a value of it.
+console.log(`Email Address: ${emailAddress}.`);//Email Address: aman.nazim07@gmail.com.
+
+//Writing "Contact No" and accessing contact varaiable to print a value of it.
+console.log(`Contact No: ${contact}.`);//Contact No: 0303 5191793.
+
+//Writing "Whatsapp Number" and accessing whatsapp number varaiable to print a value of it.
+console.log(`Whatsapp Number: ${whatsappNumber}.`);//Whatsapp Number: +92 3035191793.
+
+//Accessing hobbies variable to print "Hobbies".
+console.log(hobbies);//Hobbies:.
+
+//Writing "1-" and accessing hobby1 varaiable to print a value of it.
+console.log(`1- ${hobby1}`);//1- To learn about Technology.
+
+//Writing "2-" and accessing hobby2 varaiable to print a value of it.
+console.log(`2- ${hobby2}`);//2- Operating Computer.
+
+//Writing "3-" and accessing hobby3 varaiable to print a value of it.
+console.log(`3- ${hobby3}`);//3- Cooking.
+
+//Writing "Youtube Channel" and accessing youtubeChannel variable to print a value of it.
+console.log(`Youtube Channel: ${youtubeChannel}.`);//Youtube Channel: The Great Learner.
+
+//Writing "GitHub Account" and accessing githubAccount variable for printing its value.
+console.log(`GitHub Account: ${githubAccount}.`);//GitHub Account: Aman Nazim.
